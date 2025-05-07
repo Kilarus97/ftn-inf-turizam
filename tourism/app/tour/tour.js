e76727553b2e4fce35716541ea1d2a515c158095
Kilarus97 <zecmirko97@gmail.com>
2025-05-07 15:32:58 +0200

    feat: Dodata funkcionalnost za prikaz tura i save.

---
+++ tourism/app/tour/tour.js
@@ -0,0 +1,79 @@
+`use strict`
+
+class Tura {
+    constructor(naziv,opis,duzina,tagovi) {
+        this.naziv = naziv
+        this.opis = opis
+        this.duzina = duzina
+        this.tagovi = tagovi
+    }
+}
+
+let Tura1 = new Tura('Carski Drum','Carski Drum koji su nekada koristili sultani proteze se od Beograda do Istanbula',951,'Istorija,Istanbul,Beograd,Srednji Vek')
+let Tura2 = new Tura('Kanjon Morace','Morača je relativno mala reka,Njen kanjon je deo glavnog puta preko kog se dolazi do Podogrice',420,'Crna Gora,Priroda,Reka,Balkan')
+
+let nizTura = [Tura1,Tura2]
+
+ispisiUTabeli(nizTura)
+isisDetaljaNaKlik()
+
+
+function isisDetaljaNaKlik() {
+
+    const tabela = document.querySelector('#artikli');
+    const ispisNaziv = document.querySelector('#ispisNaziv');
+    const ispisOpis = document.querySelector('#ispisOpis');
+    const ispisDuzina = document.querySelector('#ispisDuzina');
+    const ispisTagovi = document.querySelector('#ispisTagovi');
+
+    tabela.addEventListener('click',(event) => {
+        const red = event.target.closest('tr');
+
+        if (red && red.rowIndex > 0){
+            const nazivTure = red.querySelector('td').textContent;
+            const tura = nizTura.find(a => a.naziv == nazivTure);
+                if(tura) {
+                    ispisNaziv.textContent = tura.naziv;
+                    ispisOpis.textContent = tura.opis;
+                    ispisDuzina.textContent = tura.duzina + ' KM';
+                    ispisTagovi.textContent = tura.tagovi;
+                }
+        }
+    });
+}
+
+
+function ispisiUTabeli(nizTura) {
+
+    let tabela = document.querySelector("#artikliBody")
+    tabela.innerHTML = '';
+
+
+    for(tura of nizTura){
+    let noviRed = tabela.insertRow()
+
+    let nazivCell = noviRed.insertCell()
+    nazivCell.textContent = tura.naziv
+
+    let duzinaCell = noviRed.insertCell()
+    duzinaCell.textContent = tura.duzina
+    }
+
+}
+
+function sacuvajArtikalUlokalStorage(nizTura){
+    console.log("Čuvanje u localStorage:", nizTura);
+    localStorage.setItem("ture",JSON.stringify(nizTura))
+}
+
+function ucitajArtikalIzLokalStorage(){
+    let sacuvaniPodaci = localStorage.getItem("ture")
+
+    if(sacuvaniPodaci){
+        nizTura = JSON.parse(sacuvaniPodaci)
+        console.log(nizTura)
+        ispisiUTabeli(nizTura);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", ucitajArtikalIzLokalStorage);
